44b22b1656ed3ea038f1a43d3b72127bfd79b431
Albert <n@>
2026-04-23 16:36:44 +1000

    Revert "a"
    This reverts commit c98907fc41381f83136dacf7ef77872755f9c836.

--- src/main/java/com/example/addressbook/model/ContactManager.java
+++ src/main/java/com/example/addressbook/model/ContactManager.java
@@ -3,20 +3,12 @@ package com.example.addressbook.model;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * ContactManager class manages a collection of Contact object
- */
 public class ContactManager {
     private IContactDAO contactDAO;
     public ContactManager(IContactDAO contactDAO) {
         this.contactDAO = contactDAO;
     }
 
-    /**
-     * Method to retrieve a list of contact objects
-     * @param query - The identifier to select contacts
-     * @return List of contact objects matching the query
-     */
     public List<Contact> searchContacts(String query) {
         return contactDAO.getAllContacts()
                 .stream()
@@ -33,10 +25,6 @@ public class ContactManager {
         return searchString.toLowerCase().contains(query);
     }
 
-    /**
-     * Adds a contact to the database
-     * @param contact
-     */
     public void addContact(Contact contact) {
         contactDAO.addContact(contact);
     }
